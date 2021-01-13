c6588afb38ee9143a231cff9e05f709b43fb8202
daniel_orlandi_climatempo <daniel.orlandi@climatempo.com.br>
2021-01-13 18:26:49 -0300

    (feat: artemis/core/data_extractor) Implemented Extractr class, which extrract data from request and saves at a .xlsx file, in a specific table.

--- artemis/core/data_extractor.py
+++ artemis/core/data_extractor.py
@@ -1,44 +1,118 @@
 import multiprocessing
 import numpy as np
 import pandas
+import utils
+import multiprocessing
+import os
 
-from utils import data_logger
-from itertools import chain, starmap
+from openpyxl import load_workbook
+from openpyxl.utils.dataframe import dataframe_to_rows
 
 
 class Extractor:
     def __init__(self, data_dict: dict) -> None:
         self.data_dict = data_dict
-        self.logger = data_logger.Logger().set_logger(__name__)
+        self.logger = utils.data_logger.Logger().set_logger(__name__)
 
     def get_data(self):
         return self.data_dict
 
     @staticmethod
-    def unpack(parent_key, parent_value):
-        """Unpack one level of nesting in json file"""
-        # Unpack one level only!!!
+    def set_data(data_dict: dict) -> pandas.DataFrame:
+        host = data_dict['host']
+        data = data_dict['data']
 
-        if isinstance(parent_value, dict):
-            for key, value in parent_value.items():
-                temp1 = parent_key + '_' + key
-                yield temp1, value
-        elif isinstance(parent_value, list):
-            i = 0
-            for value in parent_value:
-                temp2 = parent_key + '_'+str(i)
-                i += 1
-                yield temp2, value
+        base = pandas.DataFrame.from_dict(data, orient='index')
+
+        if (host == 'servicos.cptec.inpe.br'):
+            data = pandas.DataFrame(base['previsao'][0])
+            data['cidade'] = base['nome']['cidade']
+
+        elif(host == 'abc'):
+            pass
+
         else:
-            yield parent_key, parent_value
+            raise ValueError(
+                'host not recognized, you need to add it to the aplication. Contact development.!')
 
-    def flatten_dict(self) -> pandas.DataFrame:
-        while True:
-            # Keep unpacking the json file until all values are atomic elements (not dictionary or list)
-            self.data_dict = dict(chain.from_iterable(
-                starmap(self.unpack, self.data_dict.items())))
+        return data
 
-            # Terminate condition: not any value in the json file is dictionary or list
-            if (not any(isinstance(value, dict) for value in self.data_dict.values()) and
-                    not any(isinstance(value, list) for value in self.data_dict.values())):
-                break
+    @staticmethod
+    def to_xlsx(workbook, dataframe: pandas.DataFrame, data_type: str):
+        try:
+            if (isinstance(data_type, str)):
+                # Creates a new sheet in xlsx file with name = data_type
+                if (data_type == 'forecast'):
+                    if ('forecast' in workbook.sheetnames):
+                        pass
+                    else:
+                        sheet = workbook.create_sheet('forecast')
+
+                    sheet = workbook['forecast']
+
+                elif(data_type == 'observation'):
+                    if ('observation' in workbook.sheetnames):
+                        pass
+                    else:
+                        sheet = workbook.create_sheet('observation')
+
+                    sheet = workbook['observation']
+            else:
+                raise ValueError(f'Error while creating sheet inside workbook')
+
+            for row in dataframe_to_rows(dataframe, index=False, header=True):
+                sheet.append(row)
+
+            return workbook
+
+        except Exception as error:
+            raise error
+
+    def load_sheet(self, filename: str):
+        try:
+            self.logger.info(f'Trying to read sheet at: {filename}')
+            if (utils.check_exists(filename=filename) == False):
+                raise OSError
+
+            workbook = load_workbook(filename)
+
+            self.logger.info(f'Done.')
+            return workbook
+
+        except OSError as os_error:
+            self.logger.error(f'OS Error: {os_error}')
+            raise os_error
+
+        except Exception as error:
+            self.logger.error(f'Error: {error}')
+            raise os_error
+
+    def save_sheet(self, workbook, out_file: str):
+        try:
+            path = os.path.dirname(out_file)
+            self.logger.info(f'Saving file at: {path}')
+
+            if (utils.check_exists(dir_path=path) == False):
+                self.logger.warning(
+                    f'Folder specified to save file does not exits, creating it.')
+                os.makedirs(path)
+                self.logger.warning(f'Done.')
+
+            workbook.save(out_file)
+
+        except OSError as os_error:
+            self.logger.error(f'OS Error: {os_error}')
+            raise error
+
+        except Exception as error:
+            self.logger.error(f'Error: {error}')
+            raise error
+
+    # def run(self, num_worker: int):
+    #     queue=multiprocessing.Queue()
+    #     process=multiprocessing.Process(
+    #         target=self.set_data, args=(self.data_dict))
+    #     process.start()
+    #     process.join()
+
+    # amentar o multiprocessing
