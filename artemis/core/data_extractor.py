fba1d2700f192f9c01882234b4020367e08bfa22
daniel_orlandi_climatempo <daniel.orlandi@climatempo.com.br>
2021-02-10 23:45:04 -0300

    (core:data_extractor) Added Integration with location object to store weather data, and docstring.

--- artemis/core/data_extractor.py
+++ artemis/core/data_extractor.py
@@ -1,24 +1,72 @@
+from utils.data_logger import Logger
 import numpy as np
 import pandas
 import utils
 
 import unidecode
 
+from utils import check_d_type
 
 class Extractor:
-    def __init__(self, data_dict: dict) -> None:
-        self.data_dict = data_dict
-        self.data_frame = pandas.DataFrame
+    def __init__(self, data_dict: dict, location_list: list) -> None:
+        self.__data_dict = data_dict
+        self.__data_frame = pandas.DataFrame
+        self.__location_list = location_list
         self.logger = utils.data_logger.Logger().set_logger(__name__)
 
+    @property
     def get_data_dict(self):
-        return self.data_dict
+        #Returns data contained in __data_dict       
+        return self.__data_dict
 
+    @property
     def get_dataframe(self):
-        return self.data_frame
+        #Returns data contained in __data_frame
+        return self.__data_frame
+    
+    @property
+    def get_location_list(self):
+        #Returns data contained in __location_list
+        return self.__location_list
 
+    
+    def write_list(self, data_list, data, func):
+        for idx, item in enumerate(data_list):
+            try:
+                func(item,data[idx])
+            
+            except:
+                self.logger.info(f'No data found!')
+                func(item,str("nan"))
+            
     @staticmethod
-    def get_temp(dataframe: pandas.DataFrame, mode: str) -> pandas.DataFrame:       
+    def get_temp(dataframe: pandas.DataFrame, mode: str) -> pandas.DataFrame:
+        """
+         method responsible get a dataframe configured from set_site_data method, and get min temp
+         max temp, and site id/name. Dataframe columns should be:
+         ['DATA','TEMP_MAX', 'TEMP_MIN', 'CD_ESTACAO', 'CIDADE']
+
+        Parameters
+        ----------
+        dtaframe: pandas.Dataframe
+            dataframe containing temperature data to be extracted.
+        
+        mode: str
+            Type of temp to be selected. Accepts TEMP_MAX or TEMP_MIN
+            
+        Returns
+        ----------
+        pandas.Dataframe
+            dataframe containing selected data for given location.
+
+        Raises
+        ----------
+        TypeError
+            "mode must be a str and equal to TEMP_MAX or TEMP_MIN."    
+
+        """  
+        check_d_type(dataframe, pandas.DataFrame) 
+        check_d_type(mode, str)   
         temp_data_frame = dataframe
 
         if (isinstance(mode, str) and mode == 'TEMP_MAX'):            
@@ -50,10 +98,36 @@ class Extractor:
         else:
             raise TypeError("mode must be a str and equal to TEMP_MAX or TEMP_MIN.")
 
-        return result
+        return result    
+    
+    def set_site(self, data_dict: dict) -> pandas.DataFrame:
+        """
+         method responsible for identify the given host and location(weather station, etc), 
+         and set data retrieved acordingly. Dictionary received in this method are expected to be
+         a implemented host, and have temp_min, max, date, and site name info.
 
-    def set_data(self, data_dict: dict) -> pandas.DataFrame:
+        Parameters
+        ----------
+        data_dict: dict 
+            Dict containing location(site) data to be set
+
+        Returns
+        ----------
+        pandas.Dataframe
+            dataframe containing data for given site.
+
+        Raises
+        ----------
+        ValueError
+            'host not recognized, you need to add it to the aplication.
+             Contact development.!' 
+
+        Exception
+            catches any other error.
+
+        """
         try:
+            check_d_type(data_dict, dict)
             self.logger.info('Seting data.')
             host = data_dict['host']
             data = data_dict['data']
@@ -86,12 +160,10 @@ class Extractor:
                 
             else:
                 raise ValueError(
-                    'host not recognized, you need to add it to the aplication. Contact development.!')
+                    'host not recognized, you need to add it to the aplication. Contact development!')
 
             data['DATA'] =  pandas.to_datetime(data['DATA'], format='%Y-%m-%d')
-            data['CIDADE'] = data['CIDADE'].apply(lambda x: unidecode.unidecode(x))
-
-            self.logger.info('Location:{}'.format(data['CIDADE']))
+            data['CIDADE'] = data['CIDADE'].apply(lambda x: unidecode.unidecode(x))            
             return data
 
         except ValueError as value_error:
@@ -102,33 +174,36 @@ class Extractor:
             self.logger.error(f'Ops another error ocurred: {general_error}')
             raise general_error
 
-    def run(self) -> None:        
+    def set_locations(self) -> None:        
         self.logger.info('concatenating all results in data_dict.')
         result = pandas.DataFrame()
 
         # aplica set_data linha a linha, pq set data foi pensado para funcionar por cidade
-        for key, value in self.data_dict.items():
-            df = self.set_data(value)
-            result = pandas.concat([result, df])
+        for key, value in self.__data_dict.items():            
+            df = self.set_site(value)
+            self.logger.info('Location:{}'.format(df['CIDADE']))
+            result = pandas.concat([result, df])            
 
         result[['TEMP_MAX', 'TEMP_MIN']] = result[['TEMP_MAX', 'TEMP_MIN']].apply(pandas.to_numeric)
 
-        self.logger.info('Getting min temps.')
+        
         temp_min = self.get_temp(result, 'TEMP_MIN')
         self.logger.info('Done getting min.')
-
-        self.logger.info('Getting max temps.')
+        
         temp_max = self.get_temp(result, 'TEMP_MAX')
         self.logger.info('Done getting max.')
-
-        self.logger.info('Merging all results.')
+        
         result = pandas.merge(temp_min, temp_max, how='outer')
-        self.logger.info('Results merged!')
-
         result = result.rename(columns={"DATA_x": "DATA"})
         result.index = result['DATA']
         result.drop(columns={'DATA'})
         result.sort_index(inplace=True)
         result = result[['CIDADE', 'TEMP_MAX', "CD_ESTACAO_max", 'TEMP_MIN', "CD_ESTACAO_min"]]
-        self.data_frame = result
-        self.logger.info('Results stored!')
+        self.__data_frame = result.drop_duplicates()
+        
+        for location in self.__location_list:
+            data = self.__data_frame[self.__data_frame['CIDADE'] == location.name]
+            
+            self.write_list(location.obs_min, data['TEMP_MIN'], location.store_data)
+            self.write_list(location.obs_max, data['TEMP_MAX'], location.store_data)
+
